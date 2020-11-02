619aaabab0aec2d6ccd8426c153bd6689b27cb94
Oliver Paull <oliverp@ansto.gov.au>
2020-11-02 11:07:37 +1100

    Change function name from reduce_xrdml to reduce_xray

--- refnx/reduce/test/test_xray.py
+++ refnx/reduce/test/test_xray.py
@@ -8,4 +8,4 @@ def test_reduction_runs(data_directory):
     # just ensure that the reduction occurs without raising an Exception.
     # We're not testing for correctness here (yet)
     fpath = os.path.join(data_directory, "reduce", "180706_HA_DG2.xrdml")
-    xray.reduce_xrdml(fpath)
+    xray.reduce_xray(fpath)
